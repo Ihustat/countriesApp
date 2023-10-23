ad2bd536d737e5b0f9ffeaf2678d1843df6dd076
Ihustat <ger26dan@gmail.com>
2023-10-23 15:00:36 +0300

    get data by XMLHttpRequest

--- script.js
+++ script.js
@@ -3,4 +3,44 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-//////////////////////////////////////////////////////
+function getCountryData(country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    request.addEventListener('load', function() {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+    
+        const currencies = data.currencies;
+        const currencyName = Object.values(currencies)[0].name;
+        const currencySymbol = Object.values(currencies)[0].symbol;
+
+        const languages = data.languages;
+        const fLanguage = Object.values(languages)[0]
+
+
+        const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👨‍👩‍👧‍👦</span>${(+data.population / 1000000).toFixed(1)} millions</p>
+            <p class="country__row"><span>🗣️</span>${fLanguage}</p>
+            <p class="country__row"><span>💰</span>${currencySymbol} ${currencyName}</p>
+            </div>
+        </article>
+        `;
+    
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = '1';
+    });
+};
+
+getCountryData('russia');
+
+
+
+
+
+
